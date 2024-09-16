1c71b971be8a290f1264c6198ff1546cd44bccd6
Nighat Parveen <nighatparveen@Nighats-MacBook-Air.local>
2024-09-16 10:48:41 +0530

    snake ladder

--- snakeLadder.js
+++ snakeLadder.js
@@ -10,6 +10,17 @@ let snake = {
     64: 60,
     87: 24
 }
+
+let ladder = {
+    4: 14,
+    9: 31,
+    40: 42,
+    43: 77,
+    48: 53,
+    68: 84,
+    70: 91,
+    75: 80
+}
 function createboard() {
     let rightToLeft = true
     for(let i = 10; i > 0; i--) {
@@ -17,9 +28,16 @@ function createboard() {
         for(let j = 1; j <= 10; j++) {
             const div = document.createElement('div');
             const fillVal = `cell${i%2 === 0 ? j%2==0 ? ' fill' : '' : j%2!=0 ? ' fill' : ''}`;
-            div.className += fillVal;     
-            div.innerText = rightToLeft ? `${(i*10+1 - j)}` : `${((i*10)-10 + j)}`;
+            div.className += fillVal; 
+            const val =  rightToLeft ? `${(i*10+1 - j)}` : `${((i*10)-10 + j)}`;   
+            div.innerText = val;
 
+            if(snake[val]){
+                div.innerText+=`🐍`;
+            }
+            else if(ladder[val]){
+                div.innerText+=`🪜`;
+            }
             board.append(div);
         }
 
